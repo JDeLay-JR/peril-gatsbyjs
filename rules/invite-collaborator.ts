0e5d48609574908ab5d15f80acd063c0a5568822
James DeLay <jdelay.jr@gmail.com>
2019-04-19 16:34:36 -0400

    Gatsbot Swag Message for Contributors
    Added message for Tier 1 and Tier 2 swag. Note about 5+ merged PRs to unlock Tier 2 Swag

--- rules/invite-collaborator.ts
+++ rules/invite-collaborator.ts
@@ -5,7 +5,7 @@ Holy buckets, @${username} — we just merged your PR to Gatsby! 💪💜
 
 Gatsby is built by awesome people like you. Let us say “thanks” in two ways:
 
- 1.  **We’d like to send you some Gatsby swag.** As a token of our appreciation, you can go to the [Gatsby Swag Store][store] and log in with your GitHub account to get a coupon code good for one free piece of swag. (Currently we’ve got a couple t-shirts available, plus some socks that are really razzing our berries right now.)
+ 1.  **We’d like to send you some Gatsby swag.** As a token of our appreciation, you can go to the [Gatsby Swag Store][store] and log in with your GitHub account to get a coupon code good for one free piece of Tier 1 swag. Pick from a selection of custom Gatsby t-shirts, stickers, hats, and much more. After 5 merged PRs you will unlock one free piece of Tier 2 swag which include epic items like hoodies, pajama pants, and beverage mugs!
  2.  **We just invited you to join the Gatsby organization on GitHub.** This will add you to our team of maintainers. Accept the invite by visiting https://github.com/orgs/gatsbyjs/invitation. By joining the team, you’ll be able to label issues, review pull requests, and merge approved pull requests.
 
 If there’s anything we can do to help, please don’t hesitate to reach out to us: tweet at [@gatsbyjs][twitter] and we’ll come a-runnin’.
